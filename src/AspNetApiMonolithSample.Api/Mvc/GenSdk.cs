4b00dd51ae153795efb3f56e88b3eeb0268bc674
Jari Pennanen <ciantic@oksidi.com>
2016-07-13 11:03:19 +0300

    ...

--- src/AspNetApiMonolithSample.Api/Mvc/GenSdk.cs
+++ src/AspNetApiMonolithSample.Api/Mvc/GenSdk.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace AspNetApiMonolithSample.Api.Mvc
+namespace AspNetApiMonolithSample.Api.Swagger
 {
     internal static class SchemaExtension
     {
